303423f52ce576811bfd31ac88f565ed0ece1206
muhammad farhan <farhan3d@gmail.com>
2018-07-15 20:12:37 +0500

    fix issue
    magically closes farhan-test-org/sandbox1#6

--- canvas_move_test1.py
+++ canvas_move_test1.py
@@ -14,6 +14,9 @@ left_keyup = True
 up_keyup = True
 down_keyup = True
 
+def test_func_1():
+    pass
+
 def draw_circle(canv):
     global circles_container, test_param1
     new_circle_class = CircleClass(canv, root)
